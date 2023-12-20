13f23266dff2feecb11d93bddfe994cd19849b9a
nyirurugo omar <nyirurugoomar@gmail.com>
2023-12-20 12:14:04 +0200

    adding login error messages

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Dashboard from "./components/Dashboard/Dashboard";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import UserProfile from "./components/Dashboard/UserProfile";
 import CreateCardForm from "./components/Dashboard/CreateCardForm";
+import { SignedOut, SignedIn } from "@clerk/clerk-react";
 
 function App() {
   return (
     <>
       <Router>
         <Route exact path="/" component={Home} />
+
         <Route path="/dashboard" component={Dashboard} />
         <Route path="/profile" component={UserProfile} />
         <Route path="/create" component={CreateCardForm} />
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ClerkProvider } from "@clerk/clerk-react";
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
-
